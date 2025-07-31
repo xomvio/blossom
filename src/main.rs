2b4fc5243e77fc289d04ee06344c0c605adf464d
yasir <xomvio@proton.me>
2025-07-31 17:32:57 +0300

    added clap crate for args & improved main

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,34 @@
 use std::io;
+use clap::Parser;
 mod crypt;
 mod server;
 mod yggdrasil;
 mod app;
 use app::App;
 
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[arg(short, long)]
+    username: Option<String>,
+    #[arg(short, long)]
+    roomkey: Option<String>,
+    #[arg(short, long, default_value = "9191")]
+    port: String,
+}
+
 //building a chat app here
 fn main() -> io::Result<()> {
-    let mut username = String::new();
-    let mut roomkey = String::new();
-    let mut port = "9191".to_string();
-    let help = r#"
-Usage: program_name [OPTIONS]
 
-Options:
-  -u, --username <USERNAME>    Specify the username to display in the chat.
-  -r, --roomkey <ROOMKEY>      Specify the room key to join a chat room.
-  -p, --port <PORT>            Specify the port number to use for the connection.
-  -h, --help                   Display this help message.
-"#;
+    let cli = Cli::parse();
 
-    let mut args = std::env::args().skip(1);
-    while let Some(arg) = args.next() {
-        match arg.as_str() {
-            "--username" | "-u" => username = args.next().expect(help),
-            "--roomkey" | "-r" => roomkey = args.next().expect(help),
-            "--port" | "-p" => port = args.next().expect(help),
-            "--help" | "-h" => return Ok(println!("{}", help)),
-            _ => return Ok(println!("{}", help))
-        }
-    }
     let mut terminal = ratatui::init();
 
-    if username.is_empty() {
-        username = crypt::generate_rnd_str(10);
-    }
+    let username = cli.username.unwrap_or_else(|| crypt::generate_rnd_str(10));
 
-    let app_result = if roomkey.is_empty() {
-        //BASE64_STANDARD.encode_string(crypt::generate_roomkey(), &mut roomkey);
-        App::create_room(username, port)?.run(&mut terminal)
-    }
-    else {
-        App::join_room(username, roomkey, port)?.run(&mut terminal)
+    let app_result = match cli.roomkey {
+        Some(roomkey) => App::join_room(username, roomkey, cli.port)?.run(&mut terminal),
+        None => App::create_room(username, cli.port)?.run(&mut terminal)
     };
     
     ratatui::restore();
